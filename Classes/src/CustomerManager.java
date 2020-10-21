f0a315ebad0837d397f34af26cddab9df36dcc24
Mustafa Enes Güneruz <56077952+menesguneruz@users.noreply.github.com>
2020-10-21 15:00:47 +0300

    Add files via upload

---
+++ Classes/src/CustomerManager.java
@@ -0,0 +1,14 @@
+public class CustomerManager {
+
+    public void Add(){
+        System.out.println("Customer added!");
+    }
+
+    public void Update(){
+        System.out.println("Customer updated!");
+    }
+
+    public void Remove(){
+        System.out.println("Customer removed!");
+    }
+}
