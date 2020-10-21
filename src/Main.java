7552c9cf0f75c51c42f17ed5ed1758676a5ce0ae
Mustafa Enes Güneruz <56077952+menesguneruz@users.noreply.github.com>
2020-10-21 14:55:36 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//System.out.println("Hello World");
+
+	/* Input alma:
+	Scanner scan = new Scanner(System.in);
+
+	System.out.println("Bir sayi giriniz: ");
+	int a = scan.nextInt();
+
+	System.out.println("a değişkeninin değeri: " + a);
+	 */
+    }
+}
